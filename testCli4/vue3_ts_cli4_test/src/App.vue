df86ddca5f9f1317629f58db7c486b78d9d76504
redsky <hyun0238@gmail.com>
2020-12-08 22:39:45 +0900

    테스트 vue3

---
+++ testCli4/vue3_ts_cli4_test/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <p>{{ state.aaa }}</p>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/HelloWorld.vue";
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class App extends Vue {
+  private aaa = 'bbbbbbb';
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
