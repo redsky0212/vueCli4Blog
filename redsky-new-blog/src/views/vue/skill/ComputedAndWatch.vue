20dd097be4e36fded66cdfad9a74641e41bf35de
redsky <hyun0238@gmail.com>
2021-01-12 22:30:49 +0900

    computed정리

--- redsky-new-blog/src/views/vue/skill/ComputedAndWatch.vue
+++ redsky-new-blog/src/views/vue/skill/ComputedAndWatch.vue
@@ -16,123 +16,90 @@
               <strong>computed</strong>사용법
               <!-- span class="text-danger">(사라질 예정)</span -->
             </h2>
-            <p class="panel-subtitle">computed함수는 내부의 종속된 대상에 <code>변경이 이루어 졌을때만 실행</code>하고 바뀌지 않았을때는 캐싱되어 있다.</p>
+            <p class="panel-subtitle">computed함수는 내부의 종속된 대상에 <code>변경이 이루어 졌을때만 실행</code>하고 바뀌지 않았을때는 캐싱된다.</p>
           </header>
           <div class="panel-body">
             <ul>
               <li>
-                객체 바인딩
+                기본 예제
                 <ul>
                   <li>
-                    <p class="text-dark"><strong>'active'</strong>라는 class가 존재하려면 <strong>isActive</strong>변수값이 <strong>true</strong>이어야 한다.</p>
+                    <p class="text-dark">message 데이타 값을 변경한<strong>reversedMessage</strong>라는 computed를 사용하는 예제</p>
                   </li>
-                  <li><p class="text-dark">객체에는 여러개의 필드를 넣을 수도 있다. 예) { active: isActive, 'text-danger': hasError }</p></li>
                 </ul>
-                <pre class="prettyprint linenums">&lt;div v-bind:class="{ active: isActive }"&gt;&lt;/div&gt;</pre>
-              </li>
-              <li>
-                일반 class와 공존
-                <pre class="prettyprint linenums">&lt;div class="static" v-bind:class="{ active: isActive }"&gt;&lt;/div&gt;</pre>
-              </li>
-              <li>
-                객체 변수 바인딩
                 <pre class="prettyprint linenums">
-&lt;div v-bind:class="classObject"&gt;&lt;/div&gt;
+&lt;div id="example"&gt;
+  &lt;p&gt;원본 메시지data: &#10100;&#10100; message &#10101;&#10101;&lt;/p&gt;
+  &lt;p&gt;원본 메시지 변경한 computed사용: &#10100;&#10100; reversedMessage &#10101;&#10101;&lt;/p&gt;
+&lt;/div&gt;
 &lt;script&gt;
-// script 부분 일부.
-data: {
-  classObject: {
-    active: true,
-    'text-danger': false,
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: '안녕하세요'
   },
+  computed: {
+    // 계산된 getter
+    reversedMessage: function () {
+      // `this` 는 vm 인스턴스를 가리킵니다.
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+&lt;/script&gt;</pre
+                >
+              </li>
+              <li>
+                <code>예제 결과</code>
+                <p>message데이타를 의존하므로 reversedMessage getter함수는 message가 변경될때만 실행 됩니다. 변경되지 않으면 캐싱됩니다.</p>
+                <div class="well well-sm">
+                  <p>원본 메시지data: {{ message }}</p>
+                  <p>원본 메시지 변경한 computed사용: {{ reversedMessage }}</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- div class="panel-actions">
+              <a href="#" class="fa fa-caret-down"></a>
+              <a href="#" class="fa fa-times"></a>
+            </div -->
+
+            <h2 class="panel-title"><strong>computed</strong> 와 <strong>method</strong> 차이점</h2>
+            <p class="panel-subtitle">
+              <strong><code>캐싱 되는냐 아니냐의 차이</code></strong>
+            </p>
+          </header>
+          <div class="panel-body">
+            <ul>
+              <li>
+                <strong>reversedMessage 메소드</strong> 함수는 template이 렌더링 될때마다 항상 실행 된다.<br />
+                <code>computed와 차이점은 캐싱되느냐 아니냐의 차이 이다.</code>
+                <pre class="prettyprint linenums">
+&lt;p&gt;원본 메시지 변경한 method사용: &#10100;&#10100; reversedMessage() &#10101;&#10101;&lt;/p&gt;
+&lt;script&gt;
+// script 일부 methods 부분
+methods: {
+  reversedMessage: function () {
+    return this.message.split('').reverse().join('');
+  }
 }
 &lt;/script&gt;</pre
                 >
               </li>
               <li>
-                computed 속성 바인딩
+                <strong>항상 실행</strong> 되어야 하는 경우에는 method로 만들어 사용해야한다.
                 <pre class="prettyprint linenums">
-&lt;div v-bind:class="classObject"&gt;&lt;/div&gt;
 &lt;script&gt;
-// script 부분 일부.
-data: {
-  isActive: true,
-  error: null,
-},
-computed: {
-  classObject: function() {
-    return {
-      active: this.isActive &amp;&amp; !this.error,
-      'text-danger': this.error &amp;&amp; this.error.type === 'fatal'
-    };
+// 항상 재실행 되어야 하는 현재시간 함수는 method로 만들어야 한다.
+methods: {
+  now: function () {
+    return Date.now();
   }
-},
+}
 &lt;/script&gt;</pre
-                >
-              </li>
-              <li>
-                배열 바인딩
-                <pre class="prettyprint linenums">
-&lt;div v-bind:class="[activeClass, errorClass]"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
-data: {
-  activeClass: 'active',
-  errorClass: 'text-danger',
-},
-&lt;/script&gt;</pre
-                >
-              </li>
-              <li>
-                배열에 조건부 바인딩
-                <pre class="prettyprint linenums">
-// 삼항연산자를 이용하여 조건부 적용 방법
-&lt;div v-bind:class="[isActive? activeClass : '', errorClass]"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
-data: {
-  isActive: true,
-  activeClass: 'active',
-  errorClass: 'text-danger',
-},
-&lt;/script&gt;</pre
-                >
-              </li>
-              <li>
-                배열에 조건부 바인딩(배열내용이 많아지면 장황해지므로 객체를 사용하여 간단하게)
-                <pre class="prettyprint linenums">
-// 배열안에 내용이 많아지면 장황해지므로 객체를 이용하여 간단하게 적용할 수 있다.
-&lt;div v-bind:class="[{ active: isActive }, errorClass]"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
-data: {
-  isActive: true,
-  activeClass: 'active',
-  errorClass: 'text-danger',
-},
-&lt;/script&gt;</pre
-                >
-              </li>
-              <li>
-                싱글파일컴포넌트에 class적용하기
-                <pre class="prettyprint linenums">
-// myComponent.vue
-&lt;p class="foo bar"&gt;&lt;/p&gt;
-// 사용하는 부분
-&lt;my-component class="baz boo"&gt;&lt;/my-component&gt;
-// 결과
-&lt;p class="foo bar baz boo"&gt;&lt;/p&gt;</pre
-                >
-              </li>
-              <li>
-                싱글파일컴포넌트에 <strong>class 바인딩</strong>도 사용가능.
-                <pre class="prettyprint linenums">
-// myComponent.vue
-&lt;p class="foo bar"&gt;&lt;/p&gt;
-// 사용하는 부분
-&lt;my-component v-bind:class="{ active: isActive }"&gt;&lt;/my-component&gt;
-// 결과
-&lt;p class="foo bar active"&gt;&lt;/p&gt;</pre
                 >
               </li>
             </ul>
@@ -145,80 +112,151 @@ data: {
               <a href="#" class="fa fa-times"></a>
             </div -->
 
-            <h2 class="panel-title"><strong>style</strong> 속성에 여러가지 값 셋팅 방법</h2>
+            <h2 class="panel-title"><strong>computed</strong> 와 <strong>watch</strong>에 대하여</h2>
+            <p class="panel-subtitle">
+              <strong><code>computed</code></strong>
+              '선언형 프로그래밍',
+              <strong><code>watch</code></strong>
+              '명령형 프로그래밍'
+            </p>
           </header>
           <div class="panel-body">
             <ul>
               <li>
-                객체 바인딩
-                <ul>
-                  <li>
-                    <p class="text-dark">객체의 <strong>key</strong>는 style의 속성을 사용하되 camelCase를 사용한다. 또는 따옴표를 사용한 kebab-case를 사용한다. 예) fontSize, 'font-size'</p>
-                  </li>
-                </ul>
-                <pre class="prettyprint linenums">
-&lt;div v-bind:style="{ color: activeColor, fontSize: fontSize + 'px' }"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
-data: {
-  activeColor: 'red',
-  fontSize: 30,
-}
-&lt;/script&gt;</pre
-                >
+                <strong>명령형 프로그래밍(watch) :</strong>
+                감시할 데이타를 지정하고, 변경되면 특정함수를 실행하라.
               </li>
               <li>
-                객체 변수 바인딩
+                <strong>선언형 프로그래밍(computed) :</strong>
+                계산해야할 목표 데이타를 정의해서 계산된 결과에 따라 실행하라.
+              </li>
+              <li>
+                일반적으로 '선언형 프로그래밍'이 '명령형 프로그래밍'보다 <code>코드 반복이 적고 우수하다</code>고 평가함.
                 <pre class="prettyprint linenums">
-&lt;div v-bind:style="styleObject"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
 data: {
-  styleObject: {
-    color: 'red',
-    'font-size': '13px',
+  firstName: 'Foo',
+  lastName: 'Bar',
+  fullName: 'Foo Bar'
+},
+// watch는 코드 반복이 생김(fullName데이타선언, 같은이름의 함수 생성)
+watch: {
+  firstName: function (val) {
+    this.fullName = val + ' ' + this.lastName
   },
-}
-&lt;/script&gt;</pre
-                >
-              </li>
-              <li>
-                computed 속성 바인딩
-                <pre class="prettyprint linenums">
-&lt;div v-bind:style="styleObject"&gt;&lt;/div&gt;
-&lt;script&gt;
-// script 부분 일부.
-computed: {
-  styleObject: function() {
-    return {
-      color: 'red',
-      fontSize: '13px',
-    };
+  lastName: function (val) {
+    this.fullName = this.firstName + ' ' + val
   }
 },
-&lt;/script&gt;</pre
+// 만약 computed로 만든다면 'fullName'데이타는 필요없어짐.
+computed: {
+  fullName: function () {
+    return this.firstName + ' ' + this.lastName
+  }
+}</pre
+                >
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- div class="panel-actions">
+              <a href="#" class="fa fa-caret-down"></a>
+              <a href="#" class="fa fa-times"></a>
+            </div -->
+
+            <h2 class="panel-title"><strong>computed</strong>에 setter 함수 사용</h2>
+            <p class="panel-subtitle">
+              <strong><code>computed</code></strong>
+              속성은 기본적으로 getter함수만 있지만 필요에 따라
+              <strong><code>setter 함수</code></strong>
+              를 만들어 사용할 수 있다.
+            </p>
+          </header>
+          <div class="panel-body">
+            <ul>
+              <li>
+                setter함수를 이용하는 computed 속성 예제
+                <ul>
+                  <li>이렇게 하면 <code>this.fullName = '달라지는 값';</code> 이와같이 값을 computed에 넣어줄 수 있다.</li>
+                  <li>그러면 결과로 <code>this.firstName</code>과 <code>this.lastName</code>데이타가 업데이트 된다.</li>
+                </ul>
+                <pre class="prettyprint linenums">
+// ...
+computed: {
+  fullName: {
+    // getter
+    get: function () {
+      return this.firstName + ' ' + this.lastName
+    },
+    // setter
+    set: function (newValue) {
+      var names = newValue.split(' ')
+      this.firstName = names[0]
+      this.lastName = names[names.length - 1]
+    }
+  }
+}
+// ...</pre
                 >
               </li>
               <li>
-                배열 바인딩
-                <ul>
-                  <li>baseStyle에 overridingStyle이 추가되는 형태.</li>
-                </ul>
-                <pre class="prettyprint linenums">&lt;div v-bind:style="[baseStyle, overridingStyle]"&gt;&lt;/div&gt;</pre>
+                <strong>vue-property-decorator</strong>를 사용했을때 적용 방법
+                <pre class="prettyprint linenums">
+// ...
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class MyClass extends Vue {
+  private firstName: string = '';
+  private lastName: string = '';
+
+  private get fullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  private set fullName(newValue: string) {
+    const names = newValue.split(' ');
+    this.firstName = names[0];
+    this.lastName = names[names.length - 1];
+  }
+}
+// ...</pre
+                >
               </li>
               <li>
-                자동 접두사
-                <ul>
-                  <li>브라우저 벤더 접두어가 필요한 CSS는 Vue가 자동으로 접두어를 감지하여 스타일을 적용한다.</li>
-                </ul>
-                <pre class="prettyprint linenums" v-bind:style="{ transform: 'rotate(0.5turn)' }">&lt;div v-bind:style="{transform: 'rotate(0.5turn)'}"&gt;&lt;/div&gt;</pre>
+                <code>실행 예제</code>
+                <div class="well well-sm">
+                  <button @click="callSetterFn">fullName('John Doe') setter함수 호출</button>
+                  <p>this.firstName + this.lastName : {{ firstName + ' ' + lastName }}</p>
+                  <p>this.firstName : {{ firstName }}</p>
+                  <p>this.lastName : {{ lastName }}</p>
+                </div>
               </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- div class="panel-actions">
+              <a href="#" class="fa fa-caret-down"></a>
+              <a href="#" class="fa fa-times"></a>
+            </div -->
+
+            <h2 class="panel-title"><strong>watch</strong> 속성</h2>
+            <p class="panel-subtitle">
+              정리 필요.....
+              <a href="https://kr.vuejs.org/v2/api/#vm-watch" target="_blank">https://kr.vuejs.org/v2/api/#vm-watch</a>
+            </p>
+          </header>
+          <div class="panel-body">
+            <ul>
               <li>
-                접두사가 있는 여러 값을 배열로 넣을 수 있다.
-                <ul>
-                  <li>브라우저가 지원하는 마지막 값만 바인딩 한다.</li>
-                </ul>
-                <pre class="prettyprint linenums">&lt;div v-bind:style="{display: ['-webkit-box', '-ms-flexbox', 'flex']}"&gt;&lt;/div&gt;</pre>
+                ....
+                <pre class="prettyprint linenums">
+// ...
+// ...</pre
+                >
               </li>
             </ul>
           </div>
@@ -235,6 +273,31 @@ import { Component, Vue } from 'vue-property-decorator';
   name: 'computedAndWatch',
 })
 export default class ComputedAndWatch extends Vue {
+  private message = '안녕하세요';
+  private firstName = 'first';
+  private lastName = 'last';
+
+  private get reversedMessage() {
+    return this.message
+      .split('')
+      .reverse()
+      .join('');
+  }
+
+  private get fullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  private set fullName(newValue: string) {
+    const names = newValue.split(' ');
+    this.firstName = names[0];
+    this.lastName = names[names.length - 1];
+  }
+
+  private callSetterFn() {
+    this.fullName = 'John Doe';
+  }
+
   private mounted() {
     window.prettyPrint();
   }
