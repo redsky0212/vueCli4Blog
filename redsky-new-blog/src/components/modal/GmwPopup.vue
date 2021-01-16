20ac2b3b585ee581c0dffb08e030ce5d8b7bb2a1
redsky0212 <hyun0238@gmail.com>
2021-01-17 01:00:23 +0900

    alert예제 소스추가

---
+++ redsky-new-blog/src/components/modal/GmwPopup.vue
@@ -0,0 +1,38 @@
+<template>
+  <fnc-modal ref="modal">
+    <div slot-scope="modal" class="gmw-popup dimmed" :class="[modal.class, {'has-one-btn': (btnFrmType === '1' || btnFrmType === '2')}]">
+      <div ref="popupWrapper" class="gmw-popup-wrapper" role="alertdialog" aria-modal="true" aria-live="assertive" v-bind="_arias">
+        <div class="default-image sd_s_ex" v-show="imgUrl">
+          <img :src="imgUrl" alt="이미지" />
+        </div>
+        <div ref="title" class="title-area sd_s_ex" :class="{'title-empty': !popuTitl}">
+          <h4 tabindex="0" class="title H4_B fc_m" v-html="popuTitl"></h4>
+        </div>
+        <div ref="contents" class="gmw-popup-cont P1_M_1 fc_m" v-bind="_attrs">
+          <slot>
+            <div class="default-cont" role="text" v-html="popuCn"></div>
+          </slot>
+        </div>
+        <div class="gmw-popup-btn-area" v-if="btnFrmType === '1'">
+          <rounded-button class="layer-btn btn56 P1_B_1 fc_s sd_m" :label="mvmtBtnNm" @click="link" />
+        </div>
+        <div class="gmw-popup-btn-area" v-else-if="btnFrmType === '2'">
+          <rounded-button class="layer-btn btn56 P1_B_1 fc_s sd_m" :label="endBtnNm" @click="close" />
+        </div>
+        <div class="gmw-popup-btn-area" v-else-if="btnFrmType === '3'">
+          <button class="gmw-popup-btn sd_s_ex ntm_click" @click="close">
+            <span class="P1_M_1 fc_m_a48">{{ enBtnNm }}</span>
+          </button>
+          <button class="gmw-popup-btn sd_s_ex ntm_click" @click="link">
+            <span class="P1_M_1 fc_m">{{ mvmtBtnNm }}</span>
+          </button>
+        </div>
+        <div class="gmw-popup-btn-area" v-else-if="btnFrmType === '4'">
+          <button class="gmw-popup-btn sd_s_ex ntm_click" @click="close">
+            <span class="P1_M_1 fc_m">{{ enBtnNm }}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </fnc-modal>
+</template>
---
+++ redsky-new-blog/src/views/vue/test/AsyncAlert.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <gmw-popup ref="popup" />
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Ref } from 'vue-property-decorator';
+import { IGmwPopup, IGmwPopupParams, IGmwBottomLayer } from '@/types';
+import GmwPopup from '@/components/modal/GmwPopup.vue';
+
+@Component
+export default class App extends Vue {
+  // App.vue
+  @Ref('popup') public readonly popup!: IGmwPopup;
+
+  private created() {
+    Vue.prototype.$app = this;
+  }
+
+  public async showManagedPopup(params: IGmwPopupParams): Promise<boolean|string> {
+    this.beginDisableBackKey('어떤팝업');
+    try {
+      let result: boolean | string = false;
+      if (params.locType === 'C') {
+        result = await this.popup.show(params);
+      } else if (params.locType === 'B') {
+        result = await this.bottomLayer.show(params);
+      }
+      return result;
+    } finally {
+      this.endDisableBackKey('어떤팝업');
+    }
+  }
+}
+</script>
