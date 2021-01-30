cee9ee61536174ea842fca6899c5e80bbd17ade5
redsky <hyun0238@gmail.com>
2021-01-30 15:49:01 +0900

    kakao map type에러부분 주석처리 추가

--- redsky-new-blog/src/views/vue/test/ShortUrlTest.vue
+++ redsky-new-blog/src/views/vue/test/ShortUrlTest.vue
@@ -67,11 +67,13 @@ marker.setMap(this.mapInst2);</pre
 </template>
 
 <script lang="ts">
-import { Vue, Component, Ref } from 'vue-property-decorator';
+import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class AsyncAwaitTest extends Vue {
+  // eslint-disable-next-line
   private mapInst: any;
+  // eslint-disable-next-line
   private mapInst2: any;
 
   private mounted() {
