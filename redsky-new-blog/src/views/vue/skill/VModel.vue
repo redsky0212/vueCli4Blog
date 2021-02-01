a892fa055d4920725a70fa51b58cc53d87926bd0
redsky <hyun0238@gmail.com>
2021-02-01 22:32:31 +0900

    v-model정리중

--- redsky-new-blog/src/views/vue/skill/VModel.vue
+++ redsky-new-blog/src/views/vue/skill/VModel.vue
@@ -7,10 +7,10 @@
       <div class="col-md-12">
         <section class="panel">
           <header class="panel-heading">
-            <!-- div class="panel-actions">
-              <a href="#" class="fa fa-caret-down"></a>
-              <a href="#" class="fa fa-times"></a>
-            </div -->
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- <a href="#" class="fa fa-times"></a> -->
+            <!-- </div> -->
 
             <h2 class="panel-title">
               <strong>v-model</strong>적용하기(기본)
@@ -259,20 +259,148 @@ var vm = new Vue({
                   <br />
                   <span>Selected: {{ selected2 }}</span>
                 </div>
+                <ul>
+                  <li>
+                    <span class="text-dark"><code>v-for</code>를 이용한 <code>동적 option 데이타</code> 렌더링 예제</span>
+                  </li>
+                </ul>
+                <pre class="prettyprint linenums">
+&lt;select v-model="selected"&gt;
+  &lt;option v-for="option in options" v-bind:value="option.value"&gt;
+    &#10100;&#10100; option.text &#10101;&#10101;
+  &lt;/option&gt;
+&lt;/select&gt;
+&lt;span&gt;Selected: &#10100;&#10100; selected &#10101;&#10101;&lt;&lt;/span&gt;
+&lt;script&gt;
+new Vue({
+  el: '...',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
+  }
+})
+&lt;/script&gt;</pre
+                >
+                <div class="well well-sm">
+                  <select v-model="selected3">
+                    <option v-for="(option, index) in options3" v-bind:value="option.value" :key="index">
+                      {{ option.text }}
+                    </option>
+                  </select>
+                  <span>Selected: {{ selected3 }}</span>
+                </div>
               </li>
             </ul>
           </div>
         </section>
         <section class="panel">
           <header class="panel-heading">
-            <!-- div class="panel-actions">
-              <a href="#" class="fa fa-caret-down"></a>
-              <a href="#" class="fa fa-times"></a>
-            </div -->
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- <a href="#" class="fa fa-times"></a> -->
+            <!-- </div> -->
 
             <h2 class="panel-title">
-              <strong>...</strong>...
-              <!-- <span class="text-danger">(사라질 예정)</span> -->
+              <strong>v-model 수식어</strong>
+            </h2>
+            <p class="panel-subtitle">v-model사용 시 데이타 동기화 시점에 값 처리 추가 기능을 할 수 있다.</p>
+          </header>
+          <div class="panel-body">
+            <ul>
+              <li>
+                <h4>
+                  <strong>.lazy</strong>
+                </h4>
+                <ul>
+                  <li>
+                    <span class="text-dark"><code>v-model</code>은 입력이벤트 발생하면 바로 입력, 데이터동기화를 한다.</span>
+                  </li>
+                  <li>
+                    <span class="text-dark"><code>.lazy</code>수식어를 사용하면 <code>change 이벤트</code> 이후에 동기화 한다.</span>
+                  </li>
+                </ul>
+                <pre class="prettyprint linenums">
+// 'input' 대신 'change' 이후에 동기화 한다.
+&lt;input v-model.lazy="msg" /&gt;</pre
+                >
+              </li>
+              <li>
+                <h4>
+                  <strong>.number</strong>
+                </h4>
+                <ul>
+                  <li>
+                    <span class="text-dark">입력된 값이 <code>number type</code>이기를 원하면 시용한다.</span>
+                  </li>
+                  <li>
+                    <span class="text-dark"><code>type="number"</code>이어도 값은 항상 문자열 이기때문에 형변환을 원할때 사용.</span>
+                  </li>
+                </ul>
+                <pre class="prettyprint linenums">
+// number type으로 형변환
+&lt;input v-model.number="age" type="number" /&gt;</pre
+                >
+              </li>
+              <li>
+                <h4>
+                  <strong>.trim</strong>
+                </h4>
+                <ul>
+                  <li>
+                    <span class="text-dark">입력값을 자동으로 <code>trim</code> 되기를 원하면 사용.</span>
+                  </li>
+                </ul>
+                <pre class="prettyprint linenums">
+// 자동 trim
+&lt;input v-model.trim="age" /&gt;</pre
+                >
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- <a href="#" class="fa fa-times"></a> -->
+            <!-- </div> -->
+
+            <h2 class="panel-title">
+              <strong>v-model을 사용한 것과 사용하지 않은 것의 비교</strong>
+            </h2>
+            <p class="panel-subtitle">똑같은 기능 처리를 하지만 코드의 양과 복잡성에서 차이가 있다.</p>
+          </header>
+          <div class="panel-body">
+            <ul>
+              <li>
+                <h4>
+                  <strong>...</strong>
+                </h4>
+                <ul>
+                  <li>
+                    <span class="text-dark"><code>...</code>...</span>
+                  </li>
+                </ul>
+                <pre class="prettyprint linenums">
+// ...
+&lt;input ...&gt;</pre
+                >
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- </div> -->
+
+            <h2 class="panel-title">
+              <strong>v-model 사용 시 각 Form요소의 기본 props, event</strong>
             </h2>
             <p class="panel-subtitle">...</p>
           </header>
@@ -284,251 +412,103 @@ var vm = new Vue({
                 </h4>
                 <ul>
                   <li>
-                    <span class="text-dark"><code>plugins</code> 폴더를 생성하고 그 안에 플러그인 파일을 만든다.</span>
-                  </li>
-                  <li>
-                    <span class="text-dark">plugins 폴더를 생성하는건 강제적인건 아니다.</span>
-                  </li>
-                  <li>
-                    <span class="text-dark">Vue 플러그인은 <code>install메소드</code>를 노출 해줘야 한다.</span>
+                    <span class="text-dark"><code>...</code>...</span>
                   </li>
                 </ul>
                 <pre class="prettyprint linenums">
-// plugins/common.js
-import alert from '@/components/rayui/alert';
-// 추가...
-
-const common = {
-  install() {
-
-    // 전역 컴포넌트
-    // 전역 Dialog관련 코드 (BEGIN) ===========================
-    const popupStatusValue = {
-      key: 0, // 팝업 생성시 고유의 키값을 생성하기 위한 일련번호
-      list: [], // 생성된 팝업 컴포넌트들을 저장하는 리스트
-      popupContainerVm: null, // 팝업이 열릴 컨테이너 div의 인스턴스
-    };
-    // alert, confirm, popup이 띄워질 container div 태그 만들기.
-    Vue.component('popup-container', {
-      name: 'PopupContainer',
-      created() {
-        popupStatusValue.popupContainerVm = this;
-      },
-      render(createElement: CreateElement) {
-        const children = popupStatusValue.list.map((popup: any) => {
-          return createElement(popup.component, {
-            key: popup.key,
-            props: popup.params ? popup.params : {},
-            hook: {
-              create(_: any, vnode: VNode) {
-                popup.componentInstance = vnode.componentInstance;
-              },
-            },
-          });
-        });
-        return createElement('div', { class: 'g-popup-container', style: 'z-index:9999999;' }, children);
-      },
-    });
-    // 전역 Dialog관련 코드 (END) ==============================
-
-    // $rayui 라는 이름으로 모아서 사용하기 위하여
-    Vue.prototype.$rayui = {
-      alert,
-      // 추가...
-    };
-  },
-};
-
-export default common;</pre
+// ...
+&lt;input ...&gt;</pre
                 >
-              </li>
-              <li>
-                <h4>
-                  <strong>만든 플러그인 <code>사용등록</code>하기</strong>
-                </h4>
-                <ul>
-                  <li>
-                    <span class="text-dark">new Vue() 생성 전에 <code>Vue.use()</code>를 이용한다.</span>
-                  </li>
-                </ul>
-                <pre class="prettyprint linenums">
-// common.js공통 플러그인
-import common from './plugins/common';
-// common.install()을 호출
-Vue.use(common);
-
-new Vue({
-  // ...
-});</pre
-                >
-              </li>
-              <li>
-                <h4>
-                  <strong>전역 type등록을 위한<code>vue-prototype.d.ts</code> 파일 루트에 생성하기</strong>
-                </h4>
-                <ul>
-                  <li>
-                    <span class="text-dark">파일명이 다를경우 만든 파일 사용을 위한 등록을 <code>tsconfig.json</code>에 명시한다. ex) "include": [ "types/vue-global.d.ts" ]</span>
-                  </li>
-                </ul>
-                <pre class="prettyprint linenums">
-declare module 'vue/types/vue' {
-  // 전역 속성 type설정(Vue.$aaaaa)
-  interface VueConstructor {
-    $getPageTitle: Function,
-    $initTheme: Function,
-    $pageTitle: string | null | undefined,
-  }
-  // vue 인스턴스 type설정 (this.$aaaa)
-  interface Vue {
-    $initTheme: Function;
-    $myProperty: string | null | undefined;
-    $log: I$Log;
-    $rayui: I$Rayui;
-  }
-}
-
-// 외부 js 라이브러리 사용할때 type설정
-declare global {
-  interface Window { prettyPrint: any; }
-}</pre
-                >
-              </li>
-              <li>
-                <h4>
-                  <strong>플러그인 사용하기</strong>
-                </h4>
-                <ul>
-                  <li>
-                    <span class="text-dark">화면 script에서 사용하는 방법</span>
-                  </li>
-                </ul>
-                <pre class="prettyprint linenums">
-module.exports = {
-  mounted() {
-    // 전역 플러그인 사용
-    this.$rayui.alert();
-    // 또는
-    // 이렇게 사용하기 위해서는 interface VueConstructor에 type설정을 해줘야 빨간줄이 안뜸
-    Vue.$rayui.alert();
-  }
-}</pre
-                >
-              </li>
-              <li>
-                <h4>
-                  <strong>$rayui.alert() 참고</strong>
-                </h4>
-                <ul>
-                  <li>
-                    <span class="text-dark">alert폴더에 index.js파일로 만들고 export default (message, option) {} 이렇게 노출함.</span>
-                  </li>
-                  <li>
-                    <span class="text-dark">ui component의 alert참조</span>
-                  </li>
-                </ul>
               </li>
             </ul>
           </div>
         </section>
         <section class="panel">
           <header class="panel-heading">
-            <!-- div class="panel-actions">
-              <a href="#" class="fa fa-caret-down"></a>
-              <a href="#" class="fa fa-times"></a>
-            </div -->
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- </div> -->
 
             <h2 class="panel-title">
-              <strong>플러그인</strong>만들기(nuxt 환경에서)
-              <!-- <span class="text-danger">(사라질 예정)</span> -->
+              <strong>v-model 사용 시 문제점</strong>
             </h2>
-            <p class="panel-subtitle">nuxt를 사용하는 환경에서 플러그인 만드는 법</p>
+            <p class="panel-subtitle">...</p>
           </header>
           <div class="panel-body">
             <ul>
               <li>
                 <h4>
-                  <strong>플러그인 작성하기</strong>
+                  <strong>...</strong>
                 </h4>
                 <ul>
                   <li>
-                    <span class="text-dark">루트에 <code>plugins</code> 폴더를 생성하고 그 안에 플러그인 파일생성(plugins/rayui/index.js)</span>
-                  </li>
-                  <li>
-                    <span class="text-dark">plugins 폴더명은 정해져 있다.</span>
+                    <span class="text-dark"><code>...</code>...</span>
                   </li>
                 </ul>
                 <pre class="prettyprint linenums">
-import UiButton from '@/components/rayui/button/src/UiButton.vue';
-// alert UI컴포넌트 가져오기
-import alert from '@/components/rayui/alert';
-// 추가...
-
-export default (context, inject) => {
-  // 전역 컴포넌트 등록
-  Vue.component('ui-button', UiButton);
-
-  // $root, context 모두 플러그인 주입방식 (전역 script) 등록 ==
-  inject('rayui', {
-    alert,
-    // ...
-  });
-};</pre
+// ...
+&lt;input ...&gt;</pre
                 >
               </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- </div> -->
+
+            <h2 class="panel-title">
+              <strong>v-model을 컴포넌트에 사용하기</strong>
+            </h2>
+            <p class="panel-subtitle">...</p>
+          </header>
+          <div class="panel-body">
+            <ul>
               <li>
                 <h4>
-                  <strong>만든 플러그인 <code>사용등록</code>하기</strong>
+                  <strong>...</strong>
                 </h4>
                 <ul>
                   <li>
-                    <span class="text-dark"><code>nuxt.config.js</code>에 만든 Plugin을 등록한다.</span>
+                    <span class="text-dark"><code>...</code>...</span>
                   </li>
                 </ul>
                 <pre class="prettyprint linenums">
-// nuxt.config.js
-
-module.exports = {
-  // ...
-  plugins: [
-    '@/plugins/rayui/index.js',
-  ],
-  // ...
-};</pre
+// ...
+&lt;input ...&gt;</pre
                 >
               </li>
+            </ul>
+          </div>
+        </section>
+        <section class="panel">
+          <header class="panel-heading">
+            <!-- <div class="panel-actions"> -->
+            <!-- <a href="javascript:void(0)" class="fa fa-caret-up"></a> -->
+            <!-- </div> -->
+
+            <h2 class="panel-title">
+              <strong>vue-property-decorator 환경에서 사용하기 예제</strong>
+            </h2>
+            <p class="panel-subtitle">...</p>
+          </header>
+          <div class="panel-body">
+            <ul>
               <li>
                 <h4>
-                  <strong>플러그인 사용하기</strong>
+                  <strong>...</strong>
                 </h4>
                 <ul>
                   <li>
-                    <span class="text-dark">화면 script에서 사용하는 방법</span>
+                    <span class="text-dark"><code>...</code>...</span>
                   </li>
                 </ul>
                 <pre class="prettyprint linenums">
-module.exports = {
-  mounted() {
-    // 전역 플러그인 사용
-    this.$rayui.alert();
-    // 또는
-    Vue.$rayui.alert();
-  }
-}</pre
+// ...
+&lt;input ...&gt;</pre
                 >
-              </li>
-              <li>
-                <h4>
-                  <strong>$rayui.alert() 참고</strong>
-                </h4>
-                <ul>
-                  <li>
-                    <span class="text-dark">alert폴더에 index.js파일로 만들고 export default (message, option) {} 이렇게 노출함.</span>
-                  </li>
-                  <li>
-                    <span class="text-dark">ui component의 alert참조</span>
-                  </li>
-                </ul>
               </li>
             </ul>
           </div>
@@ -553,6 +533,12 @@ export default class VModel extends Vue {
   private selected2 = [];
   private firstName = 'first';
   private lastName = 'last';
+  private selected3 = 'A';
+  private options3 = [
+    { text: 'One', value: 'A' },
+    { text: 'Two', value: 'B' },
+    { text: 'Three', value: 'C' },
+  ];
 
   private get reversedMessage() {
     return this.message
@@ -585,3 +571,8 @@ export default class VModel extends Vue {
   }
 }
 </script>
+<style scoped>
+.panel-actions a {
+  font-size: 22px;
+}
+</style>
