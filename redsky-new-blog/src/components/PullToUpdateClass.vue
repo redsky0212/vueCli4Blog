07c78e851ae76e8adc387d71b148005869325437
redsky <hyun0238@gmail.com>
2021-11-21 15:37:32 +0900

    pullToUpdate class방식으로 수정중

--- redsky-new-blog/src/components/PullToUpdateClass.vue
+++ redsky-new-blog/src/components/PullToUpdateClass.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="pull-update-container">
-    <div></div>
+    <div class="pull-update-header" :style="{ height: `${pullDown.height}px` }">
+      <div class="pull-update-content" :style="pullDownContentStyle"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+export interface IPullDown {
+  status: number;
+  height: number;
+  msg: string;
+}
+export interface IConfig {
+  [indexd: string]: number;
+}
 
 @Component({
   components: {},
 })
-export default class PullToUpdateClass extends Vue {}
+export default class PullToUpdateClass extends Vue {
+  @Prop() readonly config!: IConfig;
+
+  private pullDown: IPullDown = {
+    status: 0,
+    height: 0,
+    msg: '',
+  };
+
+  private get pullDownContentStyle() {
+    if (this.config) {
+      return {
+        bottom: `${(this.config.pullDownHeight - 40) / 2}px`,
+      };
+    } else {
+      return '';
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -18,5 +47,13 @@ export default class PullToUpdateClass extends Vue {}
   height: 100%;
   max-height: 100%;
   overflow-y: auto;
+
+  &.pull-update-header {
+    width: 100%;
+    height: 0px;
+    overflow: hidden;
+    position: relative;
+    background-color: #2c3133;
+  }
 }
 </style>
