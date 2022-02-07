39ecca2ec4d26507f2db55c796ad137b06d248b7
redsky <hyun0238@gmail.com>
2022-02-07 20:30:14 +0900

    프로젝트 내역 업데이트

--- redsky-new-blog/src/views/project/2021/HyundaiCapitalMydata.vue
+++ redsky-new-blog/src/views/project/2021/HyundaiCapitalMydata.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <blockquote class="b-thick primary">
-      <p><strong>현대캐피탈 마이데이터 서비스 구축 (2021.09.01 ~ 2021.12.31) 4개월</strong></p>
+      <p><strong>현대캐피탈 마이데이터 서비스 구축 (2021.09.01 ~ 2022.04.30) 8개월</strong></p>
     </blockquote>
     <div class="alert alert-success">
       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
@@ -28,9 +28,11 @@
                 검증계 : <br /><br />
               </li>
               <li>현대카드 3관 6층 601</li>
-              <li>김연우PM(오토에버), 현대카드현업PM(김연우), 현업 마이데이터 개발(함승주), 최윤호(시큐센 외주)</li>
+              <li>12월까지였다가, 2월까지 연장 후, 다시 4월까지 연장</li>
+              <li>현대캐피탈 앱2.0에 mydata를 함께 올려 오픈.</li>
+              <li>정연우PM(오토에버), 현대카드현업PM(정연우), 현업 마이데이터 개발(남궁혁, 함승주, 손정현, 김보경), 최윤호(시큐센 외주)</li>
               <li>
-                현대캐피탈 앱 개발 : 최봉권PM, 한재진부장(시큐센 외주), 주성민과장(시큐센 외주), 이승현과장(삼양 외주)
+                현대캐피탈 앱(현행화) 개발 : 최봉권PM, 한재진부장(시큐센 외주), 주성민과장(시큐센 외주), 이승현과장(삼양 외주), 그 외...
               </li>
             </ul>
           </div>
